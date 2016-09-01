ab29753903d9a02b7fc395c252530d334fdb4970
Calvin Yang <calvinyang98@hotmail.com>
2016-09-01 16:15:44 +0800

    Add Block.java

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,17 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents a Person's block in the address book.
+ */
+public class Block {
+
+    private int block_;
+    
+    public Block(int block) {
+        block_ = block;
+    }
+    
+    public int getBlock() {
+        return block_;
+    }
+}
