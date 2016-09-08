53d0458ced9eb9e0df1deb30d82a4f5e8ac2d29c
Calvin Yang <calvinyang98@hotmail.com>
2016-09-08 15:54:48 +0800

    Override the getPrintableString in classes Name, Phone, Email, and Address

--- src/seedu/addressbook/data/person/Email.java
+++ src/seedu/addressbook/data/person/Email.java
@@ -6,13 +6,14 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email implements Printable{
 
     public static final String EXAMPLE = "valid@e.mail";
     public static final String MESSAGE_EMAIL_CONSTRAINTS =
             "Person emails should be 2 alphanumeric/period strings separated by '@'";
     public static final String EMAIL_VALIDATION_REGEX = "[\\w\\.]+@[\\w\\.]+";
-
+    public static final String PRINTABLE_EMAIL = "Email: %s";
+    
     public final String value;
     private boolean isPrivate;
 
@@ -57,5 +58,10 @@ public class Email {
 
     public boolean isPrivate() {
         return isPrivate;
+    }
+
+    @Override
+    public String getPrintableString() {
+        return String.format(PRINTABLE_EMAIL, value);
     }
 }
