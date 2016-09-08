6c0def80593a76cf45f502a4aff88e65b2684b37
Calvin Yang <calvinyang98@hotmail.com>
2016-09-08 17:15:17 +0800

    Add getPrintableString method to Printable.java

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -2,6 +2,19 @@ package seedu.addressbook.data.person;
 
 public interface Printable {
     
+    int PRINTABLE_STRING_START = 0;
+    int PRINTABLE_STRING_EXCESS = 2;
+
     public String getPrintableString();
     
+    /**
+     * Returns a concatenated version of the printable strings of each object.
+     */
+    default String getPrintableString(Printable... printables) {
+        StringBuilder printableString = new StringBuilder();
+        for(Printable printable: printables) {
+            printableString.append(printable.getPrintableString() + ", ");
+        }
+        return printableString.substring(PRINTABLE_STRING_START, printableString.length() - PRINTABLE_STRING_EXCESS);
+    }
 }
